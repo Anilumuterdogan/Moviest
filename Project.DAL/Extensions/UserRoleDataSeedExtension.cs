e7e5e2f7918746d63eb8ad34a59b51d5c535b7a5
Özenç Geçkin <baranx9@hotmail.com>
2023-12-28 15:46:16 +0300

    Extensions dosyasında Admin oluşturuldu.

--- Project.DAL/Extensions/UserRoleDataSeedExtension.cs
+++ Project.DAL/Extensions/UserRoleDataSeedExtension.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Project.ENTITIES.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,35 @@ namespace Project.DAL.Extensions
 {
     public static class UserRoleDataSeedExtension
     {
-        IdentityRole<int> appRole = new()
+        public static void SeedUsers(ModelBuilder modelBuilder) 
         {
+            IdentityRole<int> appRole = new()
+            {
+                 Id = 1,
+                 Name = "Admin",
+                 NormalizedName = "ADMIN",
+                 ConcurrencyStamp = Guid.NewGuid().ToString()
+            };
+            
+            modelBuilder.Entity<IdentityRole<int>>().HasData(appRole);
 
-        };
+            PasswordHasher<AppUser> passwordHasher = new PasswordHasher<AppUser>();
+
+            AppUser user = new()
+            {
+                Id = 1,
+                UserName = "aost",
+                Email = "aost@gmail.com",
+                NormalizedEmail = "AOST@GMAIL.COM",
+                NormalizedUserName = "AOST",
+                EmailConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                PasswordHash = passwordHasher.HashPassword(null, "aost")
+            };
+
+            modelBuilder.Entity<AppUser>().HasData(user);
+
+            modelBuilder.Entity<IdentityUserRole<int>>().HasData(new IdentityUserRole<int> { RoleId = appRole.Id, UserId = user.Id });
+        }
     }
 }
