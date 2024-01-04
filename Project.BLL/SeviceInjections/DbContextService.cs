f0ed4cedaf3f3db6156ae5f6207eef7b8e709ead
Anıl Umut Erdoğan <anilumuterdogan@gmail.com>
2024-01-04 14:08:44 +0300

    x

---
+++ Project.BLL/SeviceInjections/DbContextService.cs
@@ -0,0 +1,25 @@
+﻿using Castle.Core.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Project.DAL.ContextClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.SeviceInjections
+{
+    public static class DbContextService
+    {
+        public static IServiceCollection AddDbContextService(this IServiceCollection services)
+        {
+            ServiceProvider provider = services.BuildServiceProvider();
+            IConfiguration? configuration = provider.GetService<IConfiguration?>();
+
+            services.AddDbContextPool<MyContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("MyConnection")).UseLazyLoadingProxies());
+            return services;
+
+        }
+    }
+}
---
+++ Project.BLL/SeviceInjections/ManagerService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Project.BLL.ManagerServices.Abstracts;
+using Project.BLL.ManagerServices.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.SeviceInjections
+{
+    public static class ManagerService
+    {
+        public static IServiceCollection AddManagerServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IManager<,>), typeof(BaseManager<,>));
+
+            services.AddScoped<IAppUserManager, AppUserManager>();
+            services.AddScoped<ICastManager, CastManager>();
+            services.AddScoped<IGenreManager, GenreManager>();
+            services.AddScoped<IMovieCastManager, MovieCastManager>();
+            services.AddScoped<IProfileManager, ProfileManager>();
+            services.AddScoped<IMovieGenreManager, MovieGenreManager>();
+            services.AddScoped<IMovieManager, MovieManager>();
+            return services;
+        }
+    }
+}
