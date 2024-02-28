c7584318010431bf2c7209d09f136b92b7aa0d52
Özenç Geçkin <baranx9@hotmail.com>
2024-02-28 20:58:19 +0300

    x

--- Project.COREMVC/Models/AppUsers/UserRegisterModel.cs
+++ Project.COREMVC/Models/AppUsers/UserRegisterModel.cs
@@ -4,17 +4,8 @@ namespace Project.COREMVC.Models.AppUsers
 {
     public class UserRegisterModel
     {
-       // [Required(ErrorMessage = "Username field requirement")]
-        public string UserName { get; set; }
-
-        //[Required(ErrorMessage = "Password field is required")]
-        //[MinLength(3, ErrorMessage = "Min 3 characters required to be entered")]
-        public string Password { get; set; }
-
-        //[Compare("Password", ErrorMessage = "Passwords don't match")]
-        //public string ConfirmPassword { get; set; }
-
-        //[EmailAddress(ErrorMessage = "Please log in according to the email address format.")]
+        public string UserName { get; set; }    
+        public string Password { get; set; }   
         public string Email { get; set; }
 
     }
