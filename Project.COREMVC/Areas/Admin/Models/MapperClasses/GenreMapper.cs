33174c083df9ef082b4e52504136331360c897df
Anıl Umut Erdoğan <anilumuterdogan@gmail.com>
2024-04-19 01:16:30 +0300

    x

---
+++ Project.COREMVC/Areas/Admin/Models/MapperClasses/GenreMapper.cs
@@ -0,0 +1,17 @@
+﻿using Project.COREMVC.Areas.Admin.Models.Genres.PureVms;
+using Project.ENTITIES.Models;
+
+namespace Project.COREMVC.Areas.Admin.Models.MapperClasses
+{
+    public static class GenreMapper
+    {
+        public static GenreVM GetGenreVM(Genre genre)
+        {
+            return new()
+            {
+                ID = genre.ID,
+                GenreName = genre.GenreName
+            };
+        }
+    }
+}
